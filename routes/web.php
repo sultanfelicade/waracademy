fac1bc805c5b3bda8509ca91fd8ae1d560d24ad6
sultanfelycade <230411100191@student.trunojoyo.ac.id>
2025-10-12 12:26:26 +0700

    Initial commit of Laravel project

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'processRegister'])->name('register.process');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'processLogin'])->name('login.process');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('google.callback');
+
+Route::get('/home', function () {
+    if (!session()->has('pengguna_id')) {
+        return redirect()->route('login');
+    }
+    return view('home', ['username' => session('pengguna_username')]);
+})->name('home');
